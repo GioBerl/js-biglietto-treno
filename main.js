ed68a49ed640daa15d44df74b09ee8c3770076f7
Gio <giovanni.berl1@gmail.com>
2020-04-20 18:36:53 +0200

    aggiunto css

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 
 
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. 
+
 var kmDaPercorrere = prompt('quanti KM vuoi percorrere?');
 var eta = prompt('Quanti anni hai?');
 
@@ -14,14 +15,28 @@ var prezzoBiglietto = kmDaPercorrere * 0.21;
 
 // va applicato uno sconto del 20% per i minorenni (<18 anni)
 // va applicato uno sconto del 40% per gli over 65 (>=65 anni)
-if (eta > 0) {
+if (eta > 0 && eta < 100) {
     if (eta < 18) {
         var scontoGiovani = prezzoBiglietto * 0.2;
+        document.getElementById('biglietto').innerHTML = prezzoBiglietto;
+        document.getElementById('sconto').innerHTML = scontoGiovani;
+        document.getElementById('totale').innerHTML = prezzoBiglietto - scontoGiovani;
         console.log(`il prezzo del biglietto e' di ${prezzoBiglietto}€, scontato del 20% (${scontoGiovani}€), quindi dovrai pagare: ` + (prezzoBiglietto - scontoGiovani) + '€');
     } else if (eta >= 65) {
         var scontoAnziani = prezzoBiglietto * 0.4;
+        document.getElementById('biglietto').innerHTML = prezzoBiglietto;
+        document.getElementById('sconto').innerHTML = scontoAnziani;
+        document.getElementById('totale').innerHTML = prezzoBiglietto - scontoAnziani;
         console.log(`il prezzo del biglietto e' di ${prezzoBiglietto}€, scontato del 40% (${scontoAnziani}€), quindi dovrai pagare: ` + (prezzoBiglietto - scontoAnziani) + '€');
     } else {
+        document.getElementById('biglietto').innerHTML = prezzoBiglietto;
+        document.getElementById('sconto').innerHTML = '0';
+        document.getElementById('totale').innerHTML = prezzoBiglietto;
         console.log(`prezzo biglietto: ${prezzoBiglietto}€, nessuno sconto applicato`);
     }
+} else {
+    alert("OPS! Qualcosa e' andato storto");
 }
+
+
+
